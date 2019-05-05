a809716fe0dfcee8d1338b666cca1643eb0c272d
wanghao <wanghao@navercorp.com>
2019-05-05 10:42:58 +0800

    添加java版本

---
+++ src/main/java/Medium/AddTwoNumbers.java
@@ -0,0 +1,4 @@
+package main.java.Medium;
+
+public class AddTwoNumbers {
+}
--- src/main/kotlin/Easy/TwoSum.kt
+++ src/main/kotlin/Easy/TwoSum.kt
@@ -4,7 +4,9 @@ package main.kotlin.Easy
 fun twoSum(nums: IntArray,target: Int): IntArray? {
     var map:HashMap<Int,Int> = hashMapOf<Int,Int>();
     for(i in nums.indices){
-        if(map.containsKey(nums[i])) return intArrayOf(map.get(nums[i])!!,i)
+        if(map.containsKey(nums[i])) {
+            return intArrayOf(map.get(nums[i])!!,i)
+        }
         map.put(target - nums[i], i)
     }
     return null
@@ -12,7 +14,9 @@ fun twoSum(nums: IntArray,target: Int): IntArray? {
 
 fun main(){
     var result = twoSum(intArrayOf(2,8,7,3),9)
-    for(i in result!!) print(i)
+    for(i in result!!) {
+        print(i)
+    }
 }
 
 
