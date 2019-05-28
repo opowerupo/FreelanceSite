1b94fbef5516ee4b878819ac6d1c5a99e939f16f
wanghao <wanghao@navercorp.com>
2019-05-28 17:51:54 +0800

    寻找插入位置

--- src/main/kotlin/Easy/RemoveElement.kt
+++ src/main/kotlin/Easy/RemoveElement.kt
@@ -3,7 +3,7 @@ package main.kotlin.Easy
 
 fun removeElement(nums: IntArray, `val`: Int): Int {
     var i: Int = 0
-    for (j in 0..nums.size - 1) {
+    for (j in nums.indices) {
         if (nums[j] != `val`) {
             nums[i] = nums[j]
             i++
@@ -13,7 +13,18 @@ fun removeElement(nums: IntArray, `val`: Int): Int {
 }
 
 fun main() {
-    val nums = intArrayOf(1, 2, 3, 4)
-    val `val`
-    val res = removeElement()
+    val nums = intArrayOf(3, 1, 2, 2, 3, 3, 4, 3)
+    val `val` = 3
+    val res = removeElement(nums, `val`)
+    val numsStr = StringBuilder()
+    numsStr.append(res)
+    numsStr.append(" : [")
+    for (i in nums.indices) {
+        numsStr.append(nums[i])
+        numsStr.append(",")
+    }
+    numsStr.delete(numsStr.length - 1, numsStr.length)
+    numsStr.append("]")
+
+    print(numsStr)
 }
