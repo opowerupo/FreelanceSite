1f0d555c30deb742e103992db255399d79096235
opowerupo <opowerupo@126.com>
2020-08-19 16:17:11 +0800

    格式调整

--- src/main/java/Algorithm/sort/BubbleSort.java
+++ src/main/java/Algorithm/sort/BubbleSort.java
@@ -2,11 +2,52 @@ package main.java.Algorithm.sort;
 
 /**
  * 冒泡排序
+ * 时间复杂度 O(n^2)
+ * 空件复杂度 O(1) 是原地排序
+ * 稳定
  */
 public class BubbleSort {
+
+    public int[] doBubble(int[] arr){
+        int temp = 0;
+        int len = arr.length;
+        boolean hasChange = false;
+
+        //外层循环表示要冒泡的次数
+        for(int i = 0; i < len; i++){
+            //内层循环表示要比较那些元素
+            for(int j = 0; j < len - i - 1; j++){
+                //是否发生比较的标记
+                hasChange = false;
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    hasChange = true;
+                }
+            }
+            //没有发生比较提前退出循环
+            if(!hasChange){
+                break;
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args){
+        BubbleSort bs = new BubbleSort();
         int[] arr = new int[]{6,4,3,2,5,7,9,1,0};
+        arr = bs.doBubble(arr);
 
+        StringBuilder arrStr = new StringBuilder(arr.length * 2);
+        arrStr.append("[");
+        for(Integer i : arr){
+            arrStr.append(i);
+            arrStr.append(",");
+        }
+        arrStr.delete(arrStr.lastIndexOf(","),arrStr.length());
+        arrStr.append("]");
 
+        System.out.println(arrStr.toString());
     }
 }
--- src/main/java/Algorithm/sort/InsertionSort.java
+++ src/main/java/Algorithm/sort/InsertionSort.java
@@ -1,10 +1,51 @@
 package main.java.Algorithm.sort;
 
-/**
- * 插入排序
- */
+/** 插入排序
+ * 时间复杂度 O(n^2)
+ * 空件复杂度 O(1) 是原地排序
+ * 稳定
+ **/
 public class InsertionSort {
+    public int[] doInsertion(int[] arr){
+        if(arr  == null) return arr;
+        int len = arr.length;
+        if(len <= 1 ) return arr;
+
+        int value = 0;
+        int j = 0;
+        for(int i = 1; i < len; i++){
+            value = arr[i];
+            j = i - 1;
+            //数据移动，并通过比较确定数据插入位置
+            for(; j >= 0; j--){
+                if(arr[j] > value){
+                    arr[j+1] = arr[j];//向后移动数据
+                } else {
+                    //找到数据插入位置
+                    break;
+                }
+            }
+            //插入数据
+            arr[j+1] = value;
+        }
+
+        return arr;
+    }
     public static void main(String[] args){
-        int[] arr = new int[]{6,4,3,2,5,7,9,1,0};
+        InsertionSort is = new InsertionSort();
+        int[] arr = new int[]{6,4,3,2,5,7,9,1,0,8};
+
+        arr = is.doInsertion(arr);
+
+        StringBuilder arrStr = new StringBuilder(arr.length * 2);
+        arrStr.append("[");
+        for(Integer i : arr){
+            arrStr.append(i);
+            arrStr.append(",");
+        }
+        arrStr.delete(arrStr.lastIndexOf(","),arrStr.length());
+        arrStr.append("]");
+
+        System.out.println(arrStr.toString());
     }
 }
